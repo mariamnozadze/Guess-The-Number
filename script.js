6c5eb5d3017fe5b5c8113de8cb8158b16c88cc4f
mariamnozadze <mariamnozadze97@yahoo.com>
2022-08-17 22:36:01 +0400

    - Added if/else statements to display messages after inputs

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+document.querySelector(".number").textContent = 12;
+document.querySelector(".score").textContent = 10;
+document.querySelector(".guess").value = 23;
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  //   console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too high!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too low!";
+  }
+});
